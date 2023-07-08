1ff8c2230698a334d3463b29e295e77c808f1a9d
Laz <nyys811@aol.com>
2023-07-08 00:00:29 -0400

    minor changes for pushing to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 const notesDB = path.join(__dirname, 'db', 'db.json');
 
 app.use(express.urlencoded({ extended: true }));
