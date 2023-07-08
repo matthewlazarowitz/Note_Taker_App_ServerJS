5de22cb2976a59fba7f59c852b482bae6d02c243
Laz <nyys811@aol.com>
2023-07-07 23:34:51 -0400

    setup the express app in the server.js

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+const notesDB = path.join(__dirname, 'db', 'db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
