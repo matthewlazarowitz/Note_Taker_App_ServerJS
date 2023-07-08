85c82f290bc9a31a3dc984ab3c484cedaac1a7ab
Laz <nyys811@aol.com>
2023-07-07 23:42:50 -0400

    added API routes

--- server.js
+++ server.js
@@ -18,3 +18,56 @@ app.get('/', (req, res) => {
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
   });
+
+  app.get('/api/notes', (req, res) => {
+    fs.readFile(notesDB, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error retrieving notes' });
+      }
+      const notes = JSON.parse(data);
+      return res.json(notes);
+    });
+  });
+  
+  app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (!title || !text) {
+      return res.status(400).json({ error: 'Title and text are required' });
+    }
+    fs.readFile(notesDB, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error saving note' });
+      }
+      const notes = JSON.parse(data);
+      const newNote = { id: uuidv4(), title, text };
+      notes.push(newNote);
+      fs.writeFile(notesDB, JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Error saving note' });
+        }
+        return res.json(newNote);
+      });
+    });
+  });
+  
+  app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    fs.readFile(notesDB, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error deleting note' });
+      }
+      let notes = JSON.parse(data);
+      notes = notes.filter((note) => note.id !== noteId);
+      fs.writeFile(notesDB, JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Error deleting note' });
+        }
+        return res.sendStatus(204);
+      });
+    });
+  });
