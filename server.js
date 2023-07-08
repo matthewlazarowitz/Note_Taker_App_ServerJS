986fbfff835dc2b4c1d2e1571b9733da45830028
Laz <nyys811@aol.com>
2023-07-07 23:42:02 -0400

    added html routes

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ const notesDB = path.join(__dirname, 'db', 'db.json');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  });
